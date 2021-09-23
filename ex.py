0ef547f3bc2b9f1d4dd3498506e1454b4d7a538f
Diego Rodríguez <diego.rodriguez@broadcom.com>
2021-09-23 23:46:00 +0200

    c1

---
+++ ex.py
@@ -0,0 +1,100 @@
+#age = 20
+#print(age)
+#nombre = input('Nombre? ')
+#print('Te llamas ' + nombre)
+#birth_year = input('Enter your birth year ')
+# age = 2020 - int(birth_year)
+# print(age)
+# First = float(input('First: '))
+# Second = float(input('Second: '))
+# sum = str(float(First) + float(Second))
+# print('Sum: ' + sum)
+# course = 'ABC'
+# print(course.find('z'))
+# print(course.replace('B','DD'))
+# print(course)
+# print('C' in course)
+# Operators
+# print(10 + 3)
+# print(10 - 3)
+# print(10 * 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+# x = 10
+# x = x + 3
+# x += 3
+# print(x)
+# x = 3 > 2
+# print(x)
+# x = 3 == 2
+# print(x)
+# x = 3 != 2
+# print(x)
+# price = 25
+# print(price > 10 and price < 30)
+# price = 25
+# print(not price > 50)
+# temperature = 25
+# if temperature > 30:
+#     print("It's a hot day")
+#     print("Drink water")
+# elif temperature > 20:
+#     print("It's a nice day")
+# elif temperature > 10:
+#     print("It's a bit cold")
+# else:
+#     print("It's cold")
+# print('Done') #always
+# i = 1
+# while i <= 5:
+#     print(i * '*')
+#     i += 1
+# names = ["John" , "Diego" , "Lucas", "Lucia"]
+# print(names[-1])
+# print(names[-2])
+# names[0] = "Jon"
+# print(names[0])
+# print(names[0:3])
+# print(names)
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)
+# print(numbers)
+# numbers.insert(9, 8)
+# print(numbers)
+# numbers.insert(8, 7)
+# print(numbers)
+# numbers.remove()
+# print(numbers)
+# numbers.clear()
+# print(numbers)
+
+# numbers = [1, 2, 3, 4, 5]
+# print(len(numbers))
+
+# numbers = [1, 2, 3, 4, 5]
+# for item in numbers:
+#     print(item)
+# i = 0
+#
+# while i < len(numbers):
+#     print(numbers[i])
+#     i += 1
+
+# numbers = range(5, 10)
+# print(numbers)
+# for number in numbers:
+#     print(number)
+
+# numbers = range(5, 10, 2)
+# print(numbers)
+# for number in numbers:
+#     print(number)
+
+# for number in range(5):
+#     print(number)
+
+numbers = (1, 2, 3) #tuple - no se pueden cambiar
+print(numbers)
